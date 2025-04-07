d5ffa1955717f533da8bb22eefbce74398574895
Elbin12 <elbinshiby14@gmail.com>
2025-04-07 23:04:40 +0530

    Add darkMode state to userSlice and update toggle on Login component

--- frontend/src/components/Logo/Logo.jsx
+++ frontend/src/components/Logo/Logo.jsx
@@ -1,8 +1,10 @@
+import { useSelector } from "react-redux";
 
 function Logo({ className = "" }) {
+  const darkMode = useSelector(state=>state.user.darkMode)
     return (
       <img
-        src="/Logo/DarkMode_with_text.svg"
+        src={darkMode?'/Logo/DarkMode_with_text.svg':'/Logo/LightMode_with_text.svg'}
         alt="Focus One Logo"
         className={className}
       />
