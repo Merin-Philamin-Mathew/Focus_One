3025dfbcd47da20c036ed2d100d2edbb575e9d7c
Elbin12 <elbinshiby14@gmail.com>
2025-04-07 17:35:30 +0530

    Set up routing structure and  component folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
-
 import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import UserRoutes from './routes/userRoute'
+import Sample from './components/sample/sample'
+
 
 function App() {
-
-
   return (
     <>
-      <div>
-        <h2>FOCUSONE</h2>
-      </div>
+      <Router>
+        <Routes>
+          <Route path='/*' element={<UserRoutes />}/>
+          <Route path='sample/' element={<Sample />}/>
+        </Routes>
+      </Router>
     </>
   )
 }
