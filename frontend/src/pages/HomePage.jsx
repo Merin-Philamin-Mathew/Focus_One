9b30ead4a2953e1c7147e6f8090a8876c054be80
Elbin12 <elbinshiby14@gmail.com>
2025-04-08 14:03:58 +0530

    Navbar added

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/user/Navbar'
+import Home from '../components/user/Home'
+
+function HomePage() {
+  return (
+    <>
+        <Navbar />
+        <Home />
+    </>
+  )
+}
+
+export default HomePage
